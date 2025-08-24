3c48d2d67430d3ffe3531aa891122baa47239840
moslisnas <alejandromolinaquesada@gmail.com>
2025-08-24 17:54:37 +0200

    Create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home/home';
+import { Roadmap } from './roadmap/roadmap/roadmap';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home },
+    {path: 'roadmap', component: Roadmap }
+];
