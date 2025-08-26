59f5108012909a91b68fd4976dd3fa81dac41c5e
moslisnas <alejandromolinaquesada@gmail.com>
2025-08-27 00:18:46 +0200

    Redirect of the searcher implemented

--- src/app/home/searcher/searcher.ts
+++ src/app/home/searcher/searcher.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-searcher',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './searcher.html',
-  styleUrl: './searcher.css'
+  styleUrl: './searcher.css',
 })
 export class Searcher {
+  searchValue: string = '';
 
+  constructor(private router: Router) {}
+
+  redirectRoadmapPage() {
+    if (this.searchValue && this.searchValue.length > 2) {
+      this.router.navigate(['/roadmap'], {
+        queryParams: { tech: this.searchValue },
+      });
+    }
+  }
 }
