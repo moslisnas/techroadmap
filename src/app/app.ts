a3d008b93a8d0616a1deebdf952b8c926bfa87ed
moslisnas <alejandromolinaquesada@gmail.com>
2025-08-26 23:58:58 +0200

    Customize navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from "./common/nav/nav-bar/nav-bar";
 import { Footer } from "./common/footer/footer/footer";
-import { Home } from "./home/home/home";
-import { Roadmap } from "./roadmap/roadmap/roadmap";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar, Footer, Home, Roadmap],
+  imports: [RouterOutlet, NavBar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
