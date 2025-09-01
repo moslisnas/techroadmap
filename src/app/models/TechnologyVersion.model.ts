a8490075e7b3ff0b3fa8085eba5213933ea94d12
moslisnas <alejandromolinaquesada@gmail.com>
2025-09-01 18:20:41 +0200

    Add roadmap element title and description

--- src/app/models/TechnologyVersion.model.ts
+++ src/app/models/TechnologyVersion.model.ts
@@ -1,3 +1,9 @@
 export class TechnologyVersion {
-  constructor(public name: string, public description: string) {}
+  constructor(
+    public name: string,
+    public description: string,
+    public date?: string,
+    public url?: string,
+    public lts?: boolean
+  ) {}
 }
--- src/app/roadmap/roadmap-element/roadmap-element.ts
+++ src/app/roadmap/roadmap-element/roadmap-element.ts
@@ -1,5 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,ElementRef,
+  ViewChild,
+  AfterViewInit,
+  HostListener,
+  Renderer2,
+  OnDestroy } from '@angular/core';
 import { Technology } from '@app/models/Technology.model';
+import { TechnologyVersion } from '@app/models/TechnologyVersion.model';
 
 @Component({
   selector: 'app-roadmap-element',
@@ -7,6 +13,9 @@ import { Technology } from '@app/models/Technology.model';
   templateUrl: './roadmap-element.html',
   styleUrl: './roadmap-element.css',
 })
+
 export class RoadmapElement {
   @Input() tech!: Technology;
+
+  versions: TechnologyVersion[] = [];
 }
