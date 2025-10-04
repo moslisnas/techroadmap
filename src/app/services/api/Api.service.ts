cabf96eeeac0dad40c602c57f3a1dcb87b7e270f
moslisnas <alejandromolinaquesada@gmail.com>
2025-10-04 17:03:54 +0200

    Add environment configuration

--- src/app/services/api/Api.service.ts
+++ src/app/services/api/Api.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  protected apiUrl = 'http://localhost:3000/api/v0';
+  protected apiUrl = environment.apiUrl + environment.apiVersion;
 
   constructor(protected http: HttpClient) {}
 
