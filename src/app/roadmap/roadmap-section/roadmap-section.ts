f89acfe1638ed9cd493bcd975841da7bf5a2dc74
moslisnas <alejandromolinaquesada@gmail.com>
2025-10-30 23:11:11 +0100

    Fix error obtaining technologies not mocked

--- src/app/roadmap/roadmap-section/roadmap-section.ts
+++ src/app/roadmap/roadmap-section/roadmap-section.ts
@@ -13,7 +13,7 @@ export class RoadmapSection {
   @Input() techString!: string;
   @Input() techActive!: boolean;
 
-  constructor(private technologyStore: TechnologyStore, private apiService:ApiService){}
+  constructor(private technologyStore: TechnologyStore, private apiService: ApiService) {}
 
   ngOnInit() {
     if (this.techString && this.techString.length > 2) {
@@ -22,15 +22,15 @@ export class RoadmapSection {
           const techsFiltered = Object.values(technologiesData).filter((tech: any) =>
             tech.name.toLowerCase().includes(this.techString.toLowerCase())
           );
-          const techFiltered:any = techsFiltered[0];
+          const techFiltered: any = techsFiltered[0];
           // Obtain technology with all data
-          if(techFiltered && techFiltered.id){
+          if (techFiltered && techFiltered.id) {
             this.technologyStore.loadTechWithDependencies(techFiltered.id);
           }
         },
         error: (error) => {
           console.error('Error obtaining data: technologies', error);
-        }
+        },
       });
     }
   }
--- src/app/roadmap/roadmap/roadmap.ts
+++ src/app/roadmap/roadmap/roadmap.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RelatedSection } from '@app/roadmap/related-section/related-section';
 import { RoadmapSection } from '@app/roadmap/roadmap-section/roadmap-section';
-import TechMockData from 'mocks/TechData';
+import { ApiService } from '@services/api/api.service';
 
 @Component({
   selector: 'app-roadmap',
@@ -15,19 +15,26 @@ export class Roadmap {
   tech: string = '';
   filteredTech: any[] = [];
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {
     this.tech = this.route.snapshot.queryParamMap.get('tech') ?? '';
   }
 
   ngOnInit() {
     if (this.tech && this.tech.length > 2) {
-      this.filteredTech = Object.values(TechMockData).filter((tech: any) =>
-        tech.name.toLowerCase().includes(this.tech.toLowerCase())
-      );
-      if (this.filteredTech != null && this.filteredTech.length > 0) {
-        this.techActive = true;
-        this.tech = this.filteredTech[0].name;
-      }
+      this.apiService.getTechnologies().subscribe({
+        next: (technologiesData) => {
+          this.filteredTech = Object.values(technologiesData).filter((tech: any) =>
+            tech.name.toLowerCase().includes(this.tech.toLowerCase())
+          );
+          if (this.filteredTech != null && this.filteredTech.length > 0) {
+            this.techActive = true;
+            this.tech = this.filteredTech[0].name;
+          }
+        },
+        error: (error) => {
+          console.error('Error obtaining data: technologies', error);
+        },
+      });
     }
   }
 }
